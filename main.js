2bdc62a44f0c7da4af0c889518b29703525da38a
nrs32 <38587739+nrs32@users.noreply.github.com>
2020-02-08 20:10:23 -0500

    blank app with button, live-server and bootstrap linked

---
+++ main.js
@@ -0,0 +1,24 @@
+// This function is called when the Girls Who Code button is clicked
+// Because we used onClick in the HTML with the function sendAlert()
+function sendAlert() {
+
+    /* This message will appear in the console
+     * The console is visble in your browser by clicking either
+     * F12 or Ctrl + Shift + i */
+    console.log("You clicked the Girls Who Code button!")
+
+    // This lets us access and edit the div with the id alertArea
+    let alertArea = document.getElementById('alertArea');
+
+    // Show an alert on screen
+    // using innerHTML lets us write HTML code to add to the alertArea div
+    alertArea.innerHTML +=   
+        '<div class="alert alert-info alert-dismissible" role="alert">' +
+            'Code Like a Girl!' +
+
+            // This part lets us close the alert
+            '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+                '<span aria-hidden="true">&times;</span>' +
+            '</button>' +
+        '</div>';
+}
