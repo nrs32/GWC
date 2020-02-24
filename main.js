1af8d78ab47b3c7c943683a8bf1bc756ec2fad3b
nrs32 <38587739+nrs32@users.noreply.github.com>
2020-02-24 18:00:45 -0500

    add code for a simple game tile that links game in new tab

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+/**
+ * Open a game from user given gameTitle
+ * @param gameTitle String: file name to open for a given game
+ * @param extension Optional String: file extension defaults to html
+ */
+function openGame(gameTitle, extension = "html") {
+    const gameSrc = `./src/games/${gameTitle}.${extension}`;
+    window.open(gameSrc);
+}
+
 // This function is called when the Girls Who Code button is clicked
 // Because we used onClick in the HTML with the function sendAlert()
 function sendAlert() {
