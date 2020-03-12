2adc98dd334e3f9c3361e16fd87cb6dc1553053b
Nikita Sietsema <nrs32@calvin.edu>
2020-03-11 20:00:39 -0400

    start bowling game

--- main.js
+++ main.js
@@ -1,24 +1,13 @@
-// This function is called when the Girls Who Code button is clicked
-// Because we used onClick in the HTML with the function sendAlert()
-function sendAlert() {
+// listen for spaces
+function spaceListener() {
+    document.onkeyup = function(e){
+        if (e.keyCode == 32){
+            document.getElementById("space").className  = "bowlingballposition";
+        }
+    }
+}
 
-    /* This message will appear in the console
-     * The console is visble in your browser by clicking either
-     * F12 or Ctrl + Shift + i */
-    console.log("You clicked the Girls Who Code button!")
-
-    // This lets us access and edit the div with the id alertArea
-    let alertArea = document.getElementById('alertArea');
-
-    // Show an alert on screen
-    // using innerHTML lets us write HTML code to add to the alertArea div
-    alertArea.innerHTML +=   
-        '<div class="alert alert-info alert-dismissible" role="alert">' +
-            'Code Like a Girl!' +
-
-            // This part lets us close the alert
-            '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
-                '<span aria-hidden="true">&times;</span>' +
-            '</button>' +
-        '</div>';
+// reset the ball position to the starting point
+function reset() {
+    document.getElementById("space").className  = "bowlingball";
 }
